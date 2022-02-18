211f5b328737b9b15b9fae004ab644cda0b60816
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2022-02-17 20:38:51 -0400

    solving react router problem

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends React.Component{
  
   render(){
     return (
-      <Routes path='/'> 
+      <Routes path='/react-to-do-app'> 
         <Route index element={<Home
           inputToDo={this.state.inputToDo}
           handleChange={this.handleInputChange}
